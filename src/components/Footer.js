d709ccba32f9c22d6b44f9975b98017869946e4f
Vasu Chopra <vasuchopra1998@gmail.com>
2023-02-17 22:52:58 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
     <div className='footer'>
       <div className='footer-container'>
             <div className="email">
-            <a href="mailto:mdagency221@gmail.com" target="_blank"> <h4><FaMailBulk size={20} style={{ color:"#0072C6", marginRight:"2rem" }}/>
-                mdagency221@gmail.com</h4>  </a>
+            <a href="mailto:cutshotconsulting@gmail.com" target="_blank"> <h4><FaMailBulk size={20} style={{ color:"#0072C6", marginRight:"2rem" }}/>
+                cutshotconsulting@gmail.com</h4>  </a>
                 <br></br>
                 <p>Cutshot Consulting Inc.</p>
                 <br></br>
@@ -16,7 +16,7 @@ const Footer = () => {
             </div>
         </div>
             <div className="social">
-            <a href="https://www.facebook.com/cutshotconsulting/" target="_blank"> <FaFacebook size={30} style={{ color:"#3b5998", marginRight:"1rem" }}/> </a>
+            <a href="https://www.facebook.com/profile.php?id=100089648028162&mibextid=LQQJ4d" target="_blank"> <FaFacebook size={30} style={{ color:"#3b5998", marginRight:"1rem" }}/> </a>
             <a href="https://twitter.com/CutShotAgency" target="_blank">  <FaTwitter size={30} style={{ color:"#00acee", marginRight:"1rem" }}/></a>
             <a href="https://www.instagram.com/cutshotconsulting/" target="_blank"> <FaInstagram size={30} style={{ color:"#8a3ab9", marginRight:"1rem" }}/></a>
             </div>
